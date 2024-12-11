b814a1c15c3f5957bca800a012b9dbc7c4868628
Sabeeh024 <sabeehjavaid024@gmail.com>
2024-12-11 16:12:40 +0500

    Added support for fonts in Webpack Configuration - added fonts - updated styles to use fonts - updated webpack config to load fonts using asset/resource loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import '../src/style.css'
-import Icon from '../src/asset/images/download.png'
+import '../src/style.css';
+import Icon from '../src/assets/images/download.png';
 
 function component() {
     const element = document.createElement('div');
+    element.innerHTML = ['Hello', 'webpack'].join(' ');
+    element.classList.add('hello');
 
     const myIcon = new Image();
     myIcon.src = Icon;
