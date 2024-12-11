e41e353e20e46ccb274bc42566a04a7f946efa97
Sabeeh024 <sabeehjavaid024@gmail.com>
2024-12-11 15:34:43 +0500

    Adding image asset support in Webpack configuration - used webpack's asset/resource loader for loading images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import '../src/style.css'
+import Icon from '../src/asset/images/download.png'
 
-console.log("JavaScript is working! Olay");
+function component() {
+    const element = document.createElement('div');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    return element;
+}
+
+document.body.appendChild(component());
